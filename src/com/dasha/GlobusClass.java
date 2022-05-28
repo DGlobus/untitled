4b177bb692ea9a06b99f6a5f23f4efde06c671da
DGlobus <dasha.globchastaya.00@mail.ru>
2022-05-28 21:45:47 +1000

    another nonscence

--- src/com/dasha/GlobusClass.java
+++ src/com/dasha/GlobusClass.java
@@ -3,5 +3,10 @@ package com.dasha;
 public class GlobusClass {
     public GlobusClass(String some_text) {
         System.out.println("haha  "+ some_text);
+        anotherNonscence();
+    }
+
+    private void anotherNonscence() {
+        System.out.println("we need tea");
     }
 }
